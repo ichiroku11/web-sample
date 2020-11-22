5120148a488b9819d183ad2e0c4615ab6097cd6f
ichiroku11 <toolbox0x10@gmail.com>
2020-11-22 11:40:27 +0900

    Use razor pages

--- src/SampleWebApp/Startup.cs
+++ src/SampleWebApp/Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
@@ -13,6 +14,12 @@ namespace SampleWebApp {
 		// This method gets called by the runtime. Use this method to add services to the container.
 		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
 		public void ConfigureServices(IServiceCollection services) {
+			services.AddRazorPages();
+
+			services.Configure<RouteOptions>(options => {
+				options.LowercaseQueryStrings = true;
+				options.LowercaseUrls = true;
+			});
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -24,9 +31,7 @@ namespace SampleWebApp {
 			app.UseRouting();
 
 			app.UseEndpoints(endpoints => {
-				endpoints.MapGet("/", async context => {
-					await context.Response.WriteAsync("Hello World!");
-				});
+				endpoints.MapRazorPages();
 			});
 		}
 	}
