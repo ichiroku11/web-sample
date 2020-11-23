be1003401da03513bf43fdb8e4c17fa5b7e704d0
ichiroku11 <toolbox0x10@gmail.com>
2020-11-24 08:07:57 +0900

    Use Sass

--- src/SampleWebApp/webpack.config.js
+++ src/SampleWebApp/webpack.config.js
@@ -1,8 +1,41 @@
+/// <binding ProjectOpened='Watch - Development' />
 "use strict";
 
 const path = require("path");
 
-const config = {
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const FixStyleOnlyEntriesPlugin = require("webpack-fix-style-only-entries");
+
+// Sass
+const scssConfig = {
+	entry: {
+		index: path.resolve(__dirname, "styles/index.scss"),
+	},
+	module: {
+		rules: [
+			{
+				test: /\.scss$/,
+				use: [
+					MiniCssExtractPlugin.loader,
+					"css-loader",
+					"sass-loader"
+				]
+			}
+		]
+	},
+	output: {
+		path: path.resolve(__dirname, "wwwroot/css")
+	},
+	plugins: [
+		new FixStyleOnlyEntriesPlugin(),
+		new MiniCssExtractPlugin({
+			filename: "[name].css"
+		})
+	]
+};
+
+// TypeScript
+const tsConfig = {
 	entry: {
 		index: path.resolve(__dirname, "scripts/index.ts")
 	},
@@ -24,5 +57,8 @@ const config = {
 	}
 };
 
-module.exports = config;
+module.exports = [
+	scssConfig,
+	tsConfig
+];
 
