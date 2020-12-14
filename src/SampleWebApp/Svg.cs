b4e967159f68b81e4f22171c8b878ec405ca17a6
ichiroku11 <toolbox0x10@gmail.com>
2020-12-14 23:38:23 +0900

    Migrate ~/sass/*

---
+++ src/SampleWebApp/Svg.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SampleWebApp {
+	/// <summary>
+	/// SVG
+	/// </summary>
+	public enum Svg {
+		None = 0,
+
+		Archive,
+
+		AwardFill,
+
+		Cart,
+
+		ChatLeft,
+
+		ChevronLeft,
+
+		// </>
+		CodeSlash,
+
+		// 水滴、インク
+		DropletFill,
+
+		GearFill,
+
+		HandThumbsUp,
+
+		HouseDoorFill,
+
+		PersonCircle,
+
+		Star,
+
+		// ★
+		StarFill,
+
+		Sliders,
+
+		Tag,
+
+		TagsFill,
+
+		ThreeDots,
+
+		Trash,
+
+		TrophyFill,
+
+		// トラック
+		Truck,
+
+		Upload,
+	}
+}
