7764e0a1736317a293a8a6a2d996ff4210cbbfb1
ichiroku11 <toolbox0x10@gmail.com>
2021-02-10 08:14:31 +0900

    Update fadeinout.ts #2

--- src/SampleWebApp/Scripts/ts/fadeinout.ts
+++ src/SampleWebApp/Scripts/ts/fadeinout.ts
@@ -1,4 +1,58 @@
+/**
+ * 指定したミリ秒後に完了する
+ * @param delayMs
+ */
+async function delay(delayMs: number): Promise<void> {
+	return new Promise<void>((resolve, _) => {
+		window.setTimeout(() => resolve(), delayMs);
+	});
+}
 
-document.addEventListener("DOMContentLoaded", _ => {
-	alert("todo");
+/**
+ * フェードイン（指定ミリ秒間で不透明にする）する
+ * @param element 対象要素
+ * @param durationMs アニメーション期間（ミリ秒）
+ */
+async function fadeIn(element: HTMLElement, durationMs: number): Promise<void> {
+	// todo: animation
+	return new Promise<void>((resolve, _) => {
+		window.setTimeout(() => {
+			element.style.opacity = "1";
+			resolve();
+		}, durationMs);
+	});
+}
+
+/**
+ * フェードアウト（指定ミリ秒間で透明にする）する
+ * @param element 対象要素
+ * @param durationMs アニメーション期間（ミリ秒）
+ */
+async function fadeOut(element: HTMLElement, durationMs: number): Promise<void> {
+	return new Promise<void>((resolve, _) => {
+		// todo: animation
+		window.setTimeout(() => {
+			element.style.opacity = "0";
+			resolve();
+		}, durationMs);
+	});
+}
+
+document.addEventListener("DOMContentLoaded", async _ => {
+	const element = document.querySelector<HTMLDivElement>("#content");
+	if (!element) {
+		return;
+	}
+
+	element.innerText = "あいうえお";
+
+	// 最初は透明
+	element.style.opacity = "0";
+
+	// 2sでフェードイン
+	// 2s間そのまま
+	// 2sでフェードアウト
+	await fadeIn(element, 2000);
+	await delay(2000);
+	await fadeOut(element, 2000);
 });
