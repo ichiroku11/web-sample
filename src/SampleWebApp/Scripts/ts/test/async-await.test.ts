fdc2824cf478afef1f0ac8959833a55be13968eb
ichiroku11 <toolbox0x10@gmail.com>
2021-02-11 09:37:58 +0900

    Test async function

--- src/SampleWebApp/Scripts/ts/test/async-await.test.ts
+++ src/SampleWebApp/Scripts/ts/test/async-await.test.ts
@@ -12,13 +12,19 @@ import { Assert, Test } from "../unittestlib";
 // await
 // https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Operators/await
 
-
+// 値を返す
 async function test1Async() {
 	return 1;
 }
 
+// Promiseを返す
 async function test2Async() {
 	return Promise.resolve(2);
+}
+
+// 例外をスローする
+async function test3Async() {
+	throw new Error("error!");
 }
 
 
@@ -54,5 +60,20 @@ export const asyncAwaitTest = new Test("AsyncAwaitTest")
 		// promiseはPromiseオブジェクト
 		Assert.true(promise.then != undefined);
 		Assert.equal(2, value);
-	});
+	})
+	.fact("asyncawait_asyncな関数で例外をスローするとcatchできる", async () => {
+		// Arrange
+		let message = "";
 
+		// Act
+		try {
+			await test3Async();
+		} catch (ex) {
+			if (ex instanceof Error) {
+				message = ex.message;
+			}
+		}
+
+		// Assert
+		Assert.equal("error!", message);
+	});
