863808ee47b637edc1d557e24a0e8ff50e72be13
ichiroku11 <toolbox0x10@gmail.com>
2021-02-27 10:47:22 +0900

    Update ~/ts/history #4

--- src/SampleWebApp/Scripts/ts/history.ts
+++ src/SampleWebApp/Scripts/ts/history.ts
@@ -1,3 +1,35 @@
 document.addEventListener("DOMContentLoaded", _ => {
-	console.log("History API sample")
+	let page = 1;
+	const input = document.querySelector<HTMLInputElement>(".input-page")!;
+	const textarea = document.querySelector<HTMLTextAreaElement>(".textarea-state")!;
+
+	const updatePageValue = () => {
+		input.setAttribute("value", page.toString());
+	};
+
+	updatePageValue();
+
+	window.addEventListener("popstate", event => {
+		const state = (event as PopStateEvent).state;
+		const json = JSON.stringify(state);
+
+		textarea.append(json);
+		textarea.append("\n");
+	});
+
+	document.querySelector<HTMLButtonElement>(".button-push-state")?.addEventListener("click", _ => {
+		history.pushState({ page }, `page ${page}`, `?page=${page}`);
+
+		page++;
+		updatePageValue();
+	});
+
+	document.querySelector<HTMLButtonElement>(".button-replace-state")?.addEventListener("click", _ => {
+		history.replaceState({ page }, `page ${page}`, `?page=${page}`);
+
+		page++;
+		updatePageValue();
+	});
+
+	document.querySelector<HTMLButtonElement>(".button-back")?.addEventListener("click", _ => history.back());
 });
