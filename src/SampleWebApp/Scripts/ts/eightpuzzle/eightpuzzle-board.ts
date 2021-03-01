5f3ccdf52c922d4acba6acf29804a70565e48664
ichiroku11 <toolbox0x10@gmail.com>
2021-03-02 08:12:47 +0900

    Update 8 puzzle #5

--- src/SampleWebApp/Scripts/ts/eightpuzzle/eightpuzzle-board.ts
+++ src/SampleWebApp/Scripts/ts/eightpuzzle/eightpuzzle-board.ts
@@ -1,6 +1,45 @@
 
+type SlidePos = "top" | "bottom" | "left" | "right";
+
 /**
  * 8パズルボード
  */
-abstract class EightPuzzleBoard {
+export class EightPuzzleBoard {
+	public static fromJson(json: string): EightPuzzleBoard {
+		const tiles = JSON.parse(json);
+
+		if (!Array.isArray<number>(tiles)) {
+			// todo:
+			throw new Error();
+		}
+
+		return new EightPuzzleBoard(tiles);
+	}
+
+	private static validate(tiles: number[]): void {
+		// todo: 0～9までの数字が1つずつかどうか
+		if (tiles.length !== 9) {
+			// todo:
+			throw new Error();
+		}
+
+		// todo: 
+	}
+
+	private readonly _tiles: number[];
+
+	public constructor(tiles: number[]) {
+		EightPuzzleBoard.validate(tiles);
+		this._tiles = tiles;
+	}
+
+	// 空きマスの上下左右のタイルをスライドする
+	public slide(pos: SlidePos): EightPuzzleBoard {
+		// todo:
+		return this;
+	}
+
+	public toJson(): string {
+		return JSON.stringify(this._tiles);
+	}
 }
