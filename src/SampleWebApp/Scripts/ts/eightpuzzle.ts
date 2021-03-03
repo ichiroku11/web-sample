14cc94bcd73f24f8d53b0ede41d5115e78b475c6
ichiroku11 <toolbox0x10@gmail.com>
2021-03-03 23:06:57 +0900

    Update 8 puzzle #5

--- src/SampleWebApp/Scripts/ts/eightpuzzle.ts
+++ src/SampleWebApp/Scripts/ts/eightpuzzle.ts
@@ -1,6 +1,9 @@
+import { EightPuzzleBoard } from "./eightpuzzle/eightpuzzle-board";
 import { EightPuzzleTableView } from "./eightpuzzle/eightpuzzle-tableview";
 
 document.addEventListener("DOMContentLoaded", _ => {
+	const board = EightPuzzleBoard.createGaol();
+
 	const view = new EightPuzzleTableView("#eight-puzzle-view");
 	view.init();
 });
--- src/SampleWebApp/Scripts/ts/eightpuzzle/eightpuzzle-board.ts
+++ src/SampleWebApp/Scripts/ts/eightpuzzle/eightpuzzle-board.ts
@@ -1,5 +1,5 @@
 
-// 空のマス
+// 0は空きマスとする
 const eightPuzzleTileEmpty = 0;
 
 // スライドするタイル
@@ -7,21 +7,25 @@ const eightPuzzleTiles = [eightPuzzleTileEmpty, 1, 2, 3, 4, 5, 6, 7, 8] as const
 
 type EightPuzzleTile = typeof eightPuzzleTiles[number];
 
-
 // スライドするタイルの位置（空のマスが基準）
-const eightPuzzleSlidePositions = ["top", "bottom", "left", "right"] as const;
+const eightPuzzleSlideDirs = ["top", "bottom", "left", "right"] as const;
 
-type EightPuzzleSlidePosition = typeof eightPuzzleSlidePositions[number];
+export type EightPuzzleSlideDir = typeof eightPuzzleSlideDirs[number];
 
+
+type EightPuzzleBoardPos = {
+	x: number,
+	y: number,
+};
 
 
 /**
  * 8パズルボード
  */
 export class EightPuzzleBoard {
-	// 0は空きマスとする
-	public static readonly Goal = new EightPuzzleBoard([1, 2, 3, 4, 5, 6, 7, 8, 0]);
-
+	public static createGaol(): EightPuzzleBoard {
+		return new EightPuzzleBoard([1, 2, 3, 4, 5, 6, 7, 8, 0]);
+	}
 
 	public static fromJson(json: string): EightPuzzleBoard {
 		const tiles = JSON.parse(json);
@@ -51,49 +55,60 @@ export class EightPuzzleBoard {
 		this._tiles = tiles;
 	}
 
+	// 指定した位置（上下左右）の座標を取得
+	private slidePos(original: EightPuzzleBoardPos, position: EightPuzzleSlideDir): EightPuzzleBoardPos | null {
+		let result = {
+			x: original.x,
+			y: original.y
+		};
+
+		switch (position) {
+			case "top":
+				result.y--;
+				break;
+			case "left":
+				result.x--;
+				break;
+			case "right":
+				result.x++;
+				break;
+			case "bottom":
+				result.y++;
+				break;
+		}
+
+		// 範囲外
+		if (result.x < 0 || result.x >= 3 || result.y < 0 || result.y >= 3) {
+			return null;
+		}
+
+		return result;
+	}
+
 	// 空きマスの上下左右のタイルをスライドする
-	public slide(position: EightPuzzleSlidePosition): EightPuzzleBoard | null {
+	public slide(dir: EightPuzzleSlideDir): EightPuzzleBoard | null {
 		// 空きマスのインデックス
-		const indexEmpty = this._tiles.indexOf(eightPuzzleTileEmpty);
+		const emptyIndex = this._tiles.indexOf(eightPuzzleTileEmpty);
 
 		// 空きマスの(x, y)座標（x:右とy:下が正方向）
-		const empty = {
-			x: Math.floor(indexEmpty % 3),
-			y: Math.floor(indexEmpty / 3),
+		const emptyPos = {
+			x: Math.floor(emptyIndex % 3),
+			y: Math.floor(emptyIndex / 3),
 		};
 
 		// スライドするタイルの(x, y)座標
-		let tile = {
-			x: empty.x,
-			y: empty.y
-		};
-		switch (position) {
-			case "top":
-				tile.y++;
-				break;
-			case "left":
-				tile.x++;
-				break;
-			case "right":
-				tile.x++;
-				break;
-			case "bottom":
-				tile.y++;
-				break;
-		}
-
-		if (tile.x < 0 || tile.x >= 3) {
-			return null;
-		}
-		if (tile.y < 0 || tile.y >= 3) {
+		const tilePos = this.slidePos(emptyPos, dir);
+		if (!tilePos) {
 			return null;
 		}
 
 		// スライドするタイルのインデックス
-		const indexTile = tile.x + tile.y;
-		// todo:
+		const tileIndex = tilePos.x + tilePos.y * 3;
 
-		return this;
+		// 空きマスと対象のタイルを入れ替える
+		const tiles = [...this._tiles];
+		[tiles[emptyIndex], tiles[tileIndex]] = [tiles[tileIndex], tiles[emptyIndex]];
+		return new EightPuzzleBoard(tiles);
 	}
 
 	public toJson(): string {
