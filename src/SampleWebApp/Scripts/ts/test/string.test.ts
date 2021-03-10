e216e6d9707fc5980e348e5c9e796f4ad60a4dbf
ichiroku11 <toolbox0x10@gmail.com>
2021-03-11 08:07:45 +0900

    Test string.padLeft

--- src/SampleWebApp/Scripts/ts/test/string.test.ts
+++ src/SampleWebApp/Scripts/ts/test/string.test.ts
@@ -1,5 +1,21 @@
 import { Assert, Test } from "../unittestlib";
 
-// https://speakerdeck.com/mu_zaru/javascript-jin-dokinashu-kifang-es2020
+// https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/String/padStart
 
-export const stringTest = new Test("stringTest");
+export const stringTest = new Test("StringTest")
+	.fact("padStart_空白で左埋め", () => {
+		// Arrange
+		// Act
+		const actual = "1".padStart(5);
+
+		// Assert
+		Assert.equal("    1", actual);
+	})
+	.fact("padStart_指定した文字で左埋め", () => {
+		// Arrange
+		// Act
+		const actual = "1".padStart(5, "0");
+
+		// Assert
+		Assert.equal("00001", actual);
+	});
