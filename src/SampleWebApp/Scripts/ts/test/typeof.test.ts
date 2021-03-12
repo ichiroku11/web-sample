2093039cf5bdaaca1088ed7af72ce510b7bc020f
ichiroku11 <toolbox0x10@gmail.com>
2021-03-13 07:23:50 +0900

    Add typeof.test.ts

---
+++ src/SampleWebApp/Scripts/ts/test/typeof.test.ts
@@ -0,0 +1,11 @@
+import { Assert, Test } from "../unittestlib";
+
+// typeof
+// https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Operators/typeof
+
+export const typeofTest = new Test("TypeOfTest")
+	.fact("typeof_", () => {
+		// todo:
+		Assert.true(false);
+	});
+
