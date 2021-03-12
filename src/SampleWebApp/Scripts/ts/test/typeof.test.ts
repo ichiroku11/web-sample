6293156f5500ef2a28a7ad370c3d38f1ddcc46dd
ichiroku11 <toolbox0x10@gmail.com>
2021-03-13 07:28:35 +0900

    Test typeof boolean, typeof number

--- src/SampleWebApp/Scripts/ts/test/typeof.test.ts
+++ src/SampleWebApp/Scripts/ts/test/typeof.test.ts
@@ -4,8 +4,22 @@ import { Assert, Test } from "../unittestlib";
 // https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Operators/typeof
 
 export const typeofTest = new Test("TypeOfTest")
-	.fact("typeof_", () => {
+	.fact("typeof_オペランドの型を表す文字列を取得できる", () => {
+		// Arrange
+		// Act
+		// Assert
+
+		// boolean
+		Assert.equal("boolean", typeof false);
+		Assert.equal("boolean", typeof true);
+
+		// number
+		Assert.equal("number", typeof 0);
+		Assert.equal("number", typeof 1);
+		Assert.equal("number", typeof 1.0);
+		Assert.equal("number", typeof NaN);
+		Assert.equal("number", typeof Infinity);
+
 		// todo:
-		Assert.true(false);
 	});
 
