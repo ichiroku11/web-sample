ab45fa82f2e9a7cb71a4f9a36ea99e38db592c8b
ichiroku11 <toolbox0x10@gmail.com>
2021-04-01 19:48:11 +0900

    Add OptionalChainingTest

---
+++ src/SampleWebApp/Scripts/ts/test/optional-chaining.test.ts
@@ -0,0 +1,9 @@
+import { Assert, Test } from "../unittestlib";
+
+// Optional chaining(?.)
+// https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Operators/Optional_chaining
+
+export const optionalChainingTest = new Test("OptionalChainingTest")
+	.fact("", () => {
+		Assert.true(true);
+	});
