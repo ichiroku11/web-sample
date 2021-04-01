433e0ad0506f744a0142139acb266b96eae61112
ichiroku11 <toolbox0x10@gmail.com>
2021-04-02 08:17:07 +0900

    Test optional chaining property

--- src/SampleWebApp/Scripts/ts/test/optional-chaining.test.ts
+++ src/SampleWebApp/Scripts/ts/test/optional-chaining.test.ts
@@ -4,6 +4,14 @@ import { Assert, Test } from "../unittestlib";
 // https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Operators/Optional_chaining
 
 export const optionalChainingTest = new Test("OptionalChainingTest")
-	.fact("", () => {
-		Assert.true(true);
+	.fact("optionalChaining_存在しないプロパティへのアクセスでundefinedが返される", () => {
+		// Arrange
+		const obj: any = {
+		};
+
+		// Act
+		const actual = obj.property?.name;
+
+		// Assert
+		Assert.equal(undefined, actual);
 	});
