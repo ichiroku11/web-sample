c961c0310972ef7a5b8388fc5e6a81cc98d6fbe9
ichiroku11 <toolbox0x10@gmail.com>
2021-04-04 10:11:06 +0900

    Test nullish coalescing operator

--- src/SampleWebApp/Scripts/ts/test/nullish-coalescing.test.ts
+++ src/SampleWebApp/Scripts/ts/test/nullish-coalescing.test.ts
@@ -4,6 +4,13 @@ import { Assert, Test } from "../unittestlib";
 // https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Operators/Nullish_coalescing_operator
 
 export const nullishCoalescingTest = new Test("NullishCoalescingTest")
-	.fact("nullishCoalescing_", () => {
-		Assert.fail();
+	.fact("nullishCoalescing_左辺がnullの場合は右辺の値が返る", () => {
+		const value = null ?? 1;
+
+		Assert.equal(1, value);
+	})
+	.fact("nullishCoalescing_左辺がundefinedの場合は右辺の値が返る", () => {
+		const value = undefined ?? 1;
+
+		Assert.equal(1, value);
 	});
