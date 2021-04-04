d21171c4e0e3d139a7dbba7fa82f915c3b46c916
ichiroku11 <toolbox0x10@gmail.com>
2021-04-05 08:10:37 +0900

    Add object.test.ts

---
+++ src/SampleWebApp/Scripts/ts/test/object.test.ts
@@ -0,0 +1,18 @@
+import { Assert, Test } from "../unittestlib";
+
+// Object
+// https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Object
+
+export const objectTest = new Test("ObjectTest")
+	.fact("entries_", () => {
+		// todo:
+		Assert.fail();
+	})
+	.fact("keys_", () => {
+		// todo:
+		Assert.fail();
+	})
+	.fact("values_", () => {
+		// todo:
+		Assert.fail();
+	});
