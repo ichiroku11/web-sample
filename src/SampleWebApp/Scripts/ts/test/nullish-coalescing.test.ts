b7713d7765dd456b5044a665a876348ca1f80988
ichiroku11 <toolbox0x10@gmail.com>
2021-04-04 09:29:08 +0900

    Add nullish-coalescing.test.ts

---
+++ src/SampleWebApp/Scripts/ts/test/nullish-coalescing.test.ts
@@ -0,0 +1,9 @@
+import { Assert, Test } from "../unittestlib";
+
+// Null合体演算子(??) 
+// https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Operators/Nullish_coalescing_operator
+
+export const nullishCoalescingTest = new Test("NullishCoalescingTest")
+	.fact("nullishCoalescing_", () => {
+		Assert.fail();
+	});
