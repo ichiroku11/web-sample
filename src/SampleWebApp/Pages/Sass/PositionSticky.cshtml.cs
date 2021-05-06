478c6c6188f7d56150c26bf737b1d48b68ed1816
ichiroku11 <toolbox0x10@gmail.com>
2021-05-06 23:10:18 +0900

    Add cshtml, scss

---
+++ src/SampleWebApp/Pages/Sass/PositionSticky.cshtml.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SampleWebApp.Pages.Sass {
+	public class PositionStickyModel : PageModel {
+		public void OnGet() {
+		}
+	}
+}
