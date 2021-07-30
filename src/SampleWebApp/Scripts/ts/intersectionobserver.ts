e5e8f8bd7a06c6187ad70efc6f7d97313ddc341f
ichiroku11 <toolbox0x10@gmail.com>
2021-07-30 21:33:37 +0900

    Update intersectionobserver.ts

--- src/SampleWebApp/Scripts/ts/intersectionobserver.ts
+++ src/SampleWebApp/Scripts/ts/intersectionobserver.ts
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", _ => {
+	const viewport = document.querySelector("#io-viewport");
+	if (!viewport) {
+		return;
+	}
+	const target = document.querySelector("#io-target");
+	if (!target) {
+		return;
+	}
 
+	const callback = (entries: IntersectionObserverEntry[], observer: IntersectionObserver) => {
+		// todo:
+	};
+
+	const options: IntersectionObserverInit = {
+		root: viewport,
+	};
+
+	const observer = new IntersectionObserver(callback, options);
+	observer.observe(target);
 });
