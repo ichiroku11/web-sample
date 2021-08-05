ed916b1336ea38872cd8a0d7a9dda577d76691c4
ichiroku11 <toolbox0x10@gmail.com>
2021-08-06 08:05:34 +0900

    Try simple default parameter sample

--- src/SampleWebApp/Scripts/ts/test/function-default-parameter.test.ts
+++ src/SampleWebApp/Scripts/ts/test/function-default-parameter.test.ts
@@ -3,13 +3,17 @@ import { Assert, Test } from "../unittestlib";
 // デフォルト引数
 // https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Functions/Default_parameters
 
+// シンプルなサンプル
+function sample1(value = 0): number {
+	return value;
+}
+
 export const functionDefaultParameterTest = new Test("FunctionDefaultParameterTest")
-	.fact("todo", () => {
+	.fact("関数のデフォルト引数を使ってみる", () => {
 		// Arrange
 		// Act
+		const actual = sample1();
+
 		// Assert
-
-		// todo:
-
-		Assert.fail();
+		Assert.equal(0, actual);
 	});
