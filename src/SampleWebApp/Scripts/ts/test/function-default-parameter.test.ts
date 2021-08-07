f09e434bda0410288c3367f21f4fbc8c960358ac
ichiroku11 <toolbox0x10@gmail.com>
2021-08-07 09:06:08 +0900

    Update default parameter sample

--- src/SampleWebApp/Scripts/ts/test/function-default-parameter.test.ts
+++ src/SampleWebApp/Scripts/ts/test/function-default-parameter.test.ts
@@ -8,6 +8,16 @@ function sample1(value = 0): number {
 	return value;
 }
 
+// オブジェクト型のデフォルト引数
+type Sample2ParamResult = {
+	x: number,
+	y?: number,
+};
+
+function sample2(param: Sample2ParamResult = { x: 1, y: 1 }): Sample2ParamResult {
+	return param;
+}
+
 export const functionDefaultParameterTest = new Test("FunctionDefaultParameterTest")
 	.fact("関数のデフォルト引数を使ってみる", () => {
 		// Arrange
@@ -16,4 +26,26 @@ export const functionDefaultParameterTest = new Test("FunctionDefaultParameterTe
 
 		// Assert
 		Assert.equal(0, actual);
+	})
+	.fact("関数のオブジェクト型デフォルト引数を省略する", () => {
+		// Arrange
+		// Act
+		// 引数のオブジェクト自体を省略できる
+		const actual = sample2();
+
+		// Assert
+		Assert.equal(1, actual.x);
+		Assert.equal(1, actual.y);
+	})
+	.fact("関数のオブジェクト型デフォルト引数の一部のプロパティを省略することはできない", () => {
+		// Arrange
+		// Act
+		const actual = sample2({ x: 2 });
+
+		// xは省略できないためコンパイルエラー
+		//const actual = sample2({});
+
+		// Assert
+		Assert.equal(2, actual.x);
+		Assert.equal(undefined, actual.y);
 	});
