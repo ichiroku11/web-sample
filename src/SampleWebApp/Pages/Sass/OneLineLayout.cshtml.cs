f30a250e639bc816e895ae352c73ceb48493ffeb
ichiroku11 <toolbox0x10@gmail.com>
2022-01-04 09:37:00 +0900

    Use ImplicitUsings

--- src/SampleWebApp/Pages/Sass/OneLineLayout.cshtml.cs
+++ src/SampleWebApp/Pages/Sass/OneLineLayout.cshtml.cs
@@ -1,9 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace SampleWebApp.Pages.Sass;
 
--- src/SampleWebApp/Startup.cs
+++ src/SampleWebApp/Startup.cs
@@ -1,14 +1,3 @@
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Routing;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace SampleWebApp;
 
 public class Startup {
