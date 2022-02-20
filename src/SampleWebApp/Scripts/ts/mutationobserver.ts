8b53f62664307d9a9908316a9a171bf4e59f444d
ichiroku11 <toolbox0x10@gmail.com>
2022-02-21 08:00:52 +0900

    Update MutationObserver sample

--- src/SampleWebApp/Scripts/ts/mutationobserver.ts
+++ src/SampleWebApp/Scripts/ts/mutationobserver.ts
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", _ => {
-	// todo:
+	const target = document.querySelector("#mo-target");
+	if (!target) {
+		return;
+	}
+
+	const callback: MutationCallback = (mutations: MutationRecord[], _: MutationObserver) => {
+		mutations.forEach(mutation => {
+			// todo:
+			console.log(mutation);
+		});
+	};
+	const options: MutationObserverInit = {
+		childList: true
+	};
+	new MutationObserver(callback).observe(target, options);
+
+	document.querySelector<HTMLButtonElement>("#mo-button-append")?.addEventListener("click", _ => {
+		// todo:
+		alert("append");
+	});
+	document.querySelector<HTMLButtonElement>("#mo-button-remove")?.addEventListener("click", _ => {
+		// todo:
+		alert("remove");
+	});
 });
