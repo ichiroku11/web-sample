fb5ea9b815d8f0384f79132bddbacf17985dbb02
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2025-01-02 09:25:21 +0900

    Update ts

--- src/SampleWebApp/Scripts/ts/serversentevents.ts
+++ src/SampleWebApp/Scripts/ts/serversentevents.ts
@@ -2,7 +2,11 @@ document.addEventListener("DOMContentLoaded", _ => {
 	// https://developer.mozilla.org/ja/docs/Web/API/Server-sent_events/Using_server-sent_events
 	const eventSource = new EventSource("/api/sampleeventstream");
 
-	eventSource.onmessage = (event: MessageEvent) => {
-		console.log(event.data);
+	eventSource.onmessage = (event: MessageEvent<string>) => {
+		console.dir({
+			type: event.type,
+			data: event.data,
+		});
+		//console.log(event.data);
 	};
 });
