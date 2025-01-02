36d4d946ed1fbdb23118944d26ad2e3322c0a136
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2025-01-03 08:25:10 +0900

    Update

--- src/SampleWebApp/Scripts/ts/serversentevents.ts
+++ src/SampleWebApp/Scripts/ts/serversentevents.ts
@@ -1,12 +1,49 @@
 document.addEventListener("DOMContentLoaded", _ => {
 	// https://developer.mozilla.org/ja/docs/Web/API/Server-sent_events/Using_server-sent_events
-	const eventSource = new EventSource("/api/sampleeventstream");
 
-	eventSource.onmessage = (event: MessageEvent<string>) => {
-		console.dir({
-			type: event.type,
-			data: event.data,
-		});
-		//console.log(event.data);
+	const buttons = {
+		start: document.querySelector<HTMLButtonElement>("#sse-button-start"),
+		cancel: document.querySelector<HTMLButtonElement>("#sse-button-cancel"),
 	};
+	if (buttons.start === null) {
+		return;
+	}
+	if (buttons.cancel === null) {
+		return;
+	}
+
+	// todo:
+	/*
+	const progress = document.querySelector<HTMLProgressElement>("#sse-progress");
+	if (progress === null) {
+		return;
+	}
+	*/
+
+	var eventSource: EventSource | null = null;
+
+	buttons.start.addEventListener("click", _ => {
+		if (eventSource !== null) {
+			return;
+		}
+
+		eventSource = new EventSource("/api/sampleeventstream");
+
+		eventSource.onmessage = (event: MessageEvent<string>) => {
+			console.dir({
+				type: event.type,
+				data: event.data,
+			});
+			//console.log(event.data);
+		};
+	});
+
+	buttons.cancel.addEventListener("click", _ => {
+		if (eventSource === null) {
+			return;
+		}
+
+		eventSource.close();
+		eventSource = null;
+	});
 });
