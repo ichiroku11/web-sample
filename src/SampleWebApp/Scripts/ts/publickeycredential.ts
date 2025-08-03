6209cb7c9a2c4e15045417bcd211b424c74ea8d6
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2025-08-04 05:31:07 +0900

    Update PublickKeyCredential

--- src/SampleWebApp/Scripts/ts/publickeycredential.ts
+++ src/SampleWebApp/Scripts/ts/publickeycredential.ts
@@ -1,1 +1,26 @@
-﻿
+
+(async function () {
+	const tbody = document.querySelector<HTMLElement>(".pkc-table tbody");
+	if (tbody) {
+		await addTableRow(
+			tbody,
+			"isConditionalMediationAvailable",
+			(await PublicKeyCredential.isConditionalMediationAvailable()));
+		await addTableRow(
+			tbody,
+			"isUserVerifyingPlatformAuthenticatorAvailable",
+			(await PublicKeyCredential.isUserVerifyingPlatformAuthenticatorAvailable()));
+	}
+})();
+
+async function addTableRow(tbody: HTMLElement, ...values: any[]): Promise<void> {
+	const tr = document.createElement("tr");
+
+	for (const value of values) {
+		const td = document.createElement("td");
+		td.textContent = value;
+		tr.appendChild(td);
+	}
+
+	tbody.appendChild(tr);
+}
